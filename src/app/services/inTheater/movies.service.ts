417ddf7a587f1a2eb77ae504c949d651f237ead4
JancoBH <jancoboscan@gmail.com>
2022-03-23 10:34:56 -0300

    package update + new loader

--- src/app/services/inTheater/movies.service.ts
+++ src/app/services/inTheater/movies.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import 'rxjs/Rx';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
